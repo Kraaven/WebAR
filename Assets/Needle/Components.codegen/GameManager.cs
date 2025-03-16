3f17554ead497366184a7458e49d293c0b027145
Kraaven <dhruv.bhojaraj@gmail.com>
2025-03-16 20:45:54 +0530

    Added text
    - Text now shows up
    - Text can now be triggered per lab

--- Assets/Needle/Components.codegen/GameManager.cs
+++ Assets/Needle/Components.codegen/GameManager.cs
@@ -8,7 +8,13 @@ namespace Needle.Typescript.GeneratedComponents
 	public partial class GameManager : UnityEngine.MonoBehaviour
 	{
 		public UnityEngine.GameObject[] @chain_objects;
+		public UnityEngine.UI.Text @labDescriptionText;
+		public UnityEngine.RectTransform[] @allTexts;
+		public UnityEngine.UI.Text @code;
+		public bool @isVisile;
+		public bool @showingData;
 		public void PullChains(){}
+		public void ShowLabData(string @lab_name){}
 	}
 }
 
--- NTL_Posters/ViewPoster/src/scripts/manager.ts
+++ NTL_Posters/ViewPoster/src/scripts/manager.ts
@@ -1,10 +1,25 @@
-import { Animator, Behaviour, GameObject, serializable, WaitForSeconds } from "@needle-tools/engine";
+import { Animator, Behaviour, CoroutineData, GameObject, RectTransform, serializable, setActive, Text, WaitForSeconds } from "@needle-tools/engine";
+import { Vector3 } from "three";
 
 export class GameManager extends Behaviour {
-    
+
     @serializable(GameManager)
     chain_objects?: GameObject[];
 
+    @serializable(Text)
+    labDescriptionText?: Text;
+
+    @serializable(RectTransform)
+    allTexts?: RectTransform[];
+
+
+    @serializable(Text)
+    code?: Text;
+
+    isVisile?: boolean;
+    showingData?: boolean;
+
+    //private currentLabExec?: CoroutineData;
     public PullChains(): void {
         this.startCoroutine(this.pullAnimation());
     }
@@ -17,8 +32,110 @@ export class GameManager extends Behaviour {
             const animator = obj.getComponent(Animator);
             if (animator) {
                 animator.play("Pull");
-                yield  WaitForSeconds(0.85);
+                yield WaitForSeconds(0.85);
             }
         }
+
+        this.ShowLabData("Pausch");
     }
+
+    public ShowLabData(lab_name: string): void {
+        if (this.showingData) {
+            this.stopCoroutine(this.DisplayLabData(lab_name));
+        }
+        this.startCoroutine(this.DisplayLabData(lab_name));
+    }
+
+    private *DisplayLabData(lab_name: string) {
+        this.showingData = true;
+        if (!this.isVisile) {
+            this.allTexts?.forEach(element => {
+                element.scale.set(0, 0, 0);
+                setActive(element.gameObject, true);
+            });
+
+            for (let step = 0; step < 20; step++) {
+                this.allTexts?.forEach(element => {
+                element.scale.set(20 / step, 20 / step, 1);
+                });
+
+                yield WaitForSeconds(0.02);
+            }
+
+            this.isVisile = true;
+        }
+
+        if(!this.labDescriptionText) yield;
+        else{
+        switch (lab_name) {
+            case "Pausch":
+                this.labDescriptionText.text = "";
+                var data = "Title:\nFor Fucks Sake!";
+                var stepsize = 1.2/data.length;
+
+                for (let step = 0; step < data.length; step++) {
+                    this.labDescriptionText.text+=data[step];
+                    yield WaitForSeconds(stepsize); 
+                }
+                break;
+            case "Mcarthy":
+                this.labDescriptionText.text = "";
+                var data = "";
+                var stepsize = 1.2/data.length;
+
+                for (let step = 0; step < data.length; step++) {
+                    this.labDescriptionText.text+=data[step];
+                    yield WaitForSeconds(stepsize); 
+                }
+                break;
+                break;
+
+            case "Tesla":
+                this.labDescriptionText.text = "";
+                var data = "";
+                var stepsize = 1.2/data.length;
+
+                for (let step = 0; step < data.length; step++) {
+                    this.labDescriptionText.text+=data[step];
+                    yield WaitForSeconds(stepsize); 
+                }
+                break;
+                break;
+
+            case "Satoshi":
+                this.labDescriptionText.text = "";
+                var data = "";
+                var stepsize = 1.2/data.length;
+
+                for (let step = 0; step < data.length; step++) {
+                    this.labDescriptionText.text+=data[step];
+                    yield WaitForSeconds(stepsize); 
+                }
+                break;
+                break;
+
+            case "Norman":
+                this.labDescriptionText.text = "";
+                var data = "";
+                var stepsize = 1.2/data.length;
+
+                for (let step = 0; step < data.length; step++) {
+                    this.labDescriptionText.text+=data[step];
+                    yield WaitForSeconds(stepsize); 
+                }
+                break;
+                break;
+
+
+
+            default:
+                break;
+        }
+    }
+
+
+
+    }
+
+
 }
